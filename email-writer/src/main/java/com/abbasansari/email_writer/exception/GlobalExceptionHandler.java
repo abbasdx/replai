e9a248e641efef7b2c89ce55ec8035d8b4e43b87
Abbas Ansari <iamabbas.self@gmail.com>
2026-02-15 18:06:28 +0530

    add global exception handler

---
+++ email-writer/src/main/java/com/abbasansari/email_writer/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.abbasansari.email_writer.exception;
+
+import com.abbasansari.email_writer.model.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse> handleValidationException(
+            MethodArgumentNotValidException ex) {
+
+        String message = ex.getBindingResult()
+                .getFieldError()
+                .getDefaultMessage();
+
+        return ResponseEntity.badRequest()
+                .body(new ApiResponse(false, message, null));
+    }
+}
