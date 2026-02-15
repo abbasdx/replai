df9142d74c98bdd820b57c05afabd545f1690fc6
Abbas Ansari <iamabbas.self@gmail.com>
2026-02-15 17:37:34 +0530

    update email request

--- email-writer/src/main/java/com/abbasansari/email_writer/model/EmailRequest.java
+++ email-writer/src/main/java/com/abbasansari/email_writer/model/EmailRequest.java
@@ -1,9 +1,14 @@
 package com.abbasansari.email_writer.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class EmailRequest {
+
+    @NotBlank(message = "Email content must not be empty")
+    @Size(min = 30, max = 3000, message = "Email content must be between 30 and 2000 characters")
     private String emailContent;
     private String tone;
 }
